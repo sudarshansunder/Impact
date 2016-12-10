a3bb2c9403fc0daa4df6f62d8a0223f983b18016
Sudarshan Sunder <sudarshan@buzzfactr.co>
2016-12-11 02:29:01 +0530

    Login and Registration

--- app/src/main/java/com/adityawalvekar/impact/impact/EventCreatorFragment.java
+++ app/src/main/java/com/adityawalvekar/impact/impact/EventCreatorFragment.java
@@ -1,18 +1,14 @@
 package com.adityawalvekar.impact.impact;
 
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.renderscript.ScriptGroup;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.ButtonBarLayout;
 import android.text.Editable;
 import android.text.InputType;
 import android.util.Base64;
@@ -38,7 +34,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -46,69 +41,21 @@ import java.util.Map;
 import static android.app.Activity.RESULT_OK;
 import static android.content.Context.MODE_PRIVATE;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EventCreatorFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EventCreatorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EventCreatorFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    final Calendar calendar = Calendar.getInstance();
     ImageView imageView;
     String base64 = "";
     EditText eventDateEditText;
 
-    final Calendar calendar = Calendar.getInstance();
-
-    private OnFragmentInteractionListener mListener;
-
     public EventCreatorFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventCreatorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventCreatorFragment newInstance(String param1, String param2) {
-        EventCreatorFragment fragment = new EventCreatorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View rootView =  inflater.inflate(R.layout.fragment_event_creator, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_event_creator, container, false);
         imageView = (ImageView) rootView.findViewById(R.id.eventCreatorImage);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,7 +66,7 @@ public class EventCreatorFragment extends Fragment {
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), 6969);
             }
         });
-        base64 = new String();
+        base64 = "";
         Button button = (Button) rootView.findViewById(R.id.createEventButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,8 +74,8 @@ public class EventCreatorFragment extends Fragment {
                 EditText eventNameEditText = (EditText) rootView.findViewById(R.id.eventCreatorName);
                 EditText eventDescriptionEditText = (EditText) rootView.findViewById(R.id.eventCreatorDescription);
                 EditText eventAddressEditText = (EditText) rootView.findViewById(R.id.eventCreatorAddress);
-                if(eventNameEditText.getText().toString().matches("")||eventDescriptionEditText.getText().toString().matches("")||eventAddressEditText.getText().toString().matches("")) {
-                    Toast.makeText(getActivity(),"Please enter data in all the fields",Toast.LENGTH_SHORT).show();
+                if (eventNameEditText.getText().toString().matches("") || eventDescriptionEditText.getText().toString().matches("") || eventAddressEditText.getText().toString().matches("")) {
+                    Toast.makeText(getActivity(), "Please enter data in all the fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 Editable editable1 = eventNameEditText.getText();
@@ -137,12 +84,12 @@ public class EventCreatorFragment extends Fragment {
                 String eventName = editable1.toString();
                 String eventDescription = editable2.toString();
                 String eventAddress = editable3.toString();
-                if(eventDateEditText.getText().toString().matches("")) {
-                    Toast.makeText(getActivity(),"Please choose a Date",Toast.LENGTH_SHORT).show();
+                if (eventDateEditText.getText().toString().matches("")) {
+                    Toast.makeText(getActivity(), "Please choose a Date", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(base64.matches("")) {
-                    Toast.makeText(getActivity(),"Please pick an Image",Toast.LENGTH_SHORT).show();
+                if (base64.matches("")) {
+                    Toast.makeText(getActivity(), "Please pick an Image", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 long currTime = calendar.getTime().getTime();
@@ -163,35 +110,35 @@ public class EventCreatorFragment extends Fragment {
         eventDateEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog(getActivity(),date,calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog(getActivity(), date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
         return rootView;
     }
 
-    private void updateLabel(){
+    private void updateLabel() {
         String myFormat = "MM/dd/yy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myFormat, Locale.US);
         eventDateEditText.setText(simpleDateFormat.format(calendar.getTime()));
     }
 
-    public void createEvent(final String eventName, final String eventDescription, final String eventAddress, String img, final long currTime, final View view){
+    public void createEvent(final String eventName, final String eventDescription, final String eventAddress, String img, final long currTime, final View view) {
         RequestQueue requestQueue = Volley.newRequestQueue(this.getActivity());
         StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://impact.adityawalvekar.com/event", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Snackbar.make(view,"Event was Created", Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(view, "Event was Created", Snackbar.LENGTH_SHORT).show();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.v("EventCreatorFragment","Error creating event");
+                Log.v("EventCreatorFragment", "Error creating event");
             }
-        }){
+        }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                HashMap<String, String> hashMap = new HashMap<String,String>();
-                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("user_data",MODE_PRIVATE);
+                HashMap<String, String> hashMap = new HashMap<String, String>();
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("user_data", MODE_PRIVATE);
                 String userName = sharedPreferences.getString("username", "");
                 hashMap.put("username", userName);
                 hashMap.put("title", eventName);
@@ -225,27 +172,5 @@ public class EventCreatorFragment extends Fragment {
                 e.printStackTrace();
             }
         }
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/adityawalvekar/impact/impact/ProfileFragment.java
+++ app/src/main/java/com/adityawalvekar/impact/impact/ProfileFragment.java
@@ -1,9 +1,5 @@
 package com.adityawalvekar.impact.impact;
 
-import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,13 +9,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.BarLineChartBase;
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -27,56 +20,9 @@ import com.github.mikephil.charting.data.PieEntry;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ProfileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -85,20 +31,20 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         PieChart pieChart = (PieChart) rootView.findViewById(R.id.regularityPieChart);
-        ArrayList<PieEntry> entries =  new ArrayList<>();
-        entries.add(new PieEntry(95f,"Impact Created"));
-        entries.add(new PieEntry(5f,"Missed"));
-        PieDataSet dataSet = new PieDataSet(entries,"Regularity");
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(95f, "Impact Created"));
+        entries.add(new PieEntry(5f, "Missed"));
+        PieDataSet dataSet = new PieDataSet(entries, "Regularity");
         PieData pieData = new PieData(dataSet);
         pieChart.setData(pieData);
         pieChart.invalidate();
 
         BarChart barChart = (BarChart) rootView.findViewById(R.id.contributionBarChart);
         List<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1f,30f));
-        barEntries.add(new BarEntry(2f,40f));
-        barEntries.add(new BarEntry(3f,10f));
-        barEntries.add(new BarEntry(4f,50f));
+        barEntries.add(new BarEntry(1f, 30f));
+        barEntries.add(new BarEntry(2f, 40f));
+        barEntries.add(new BarEntry(3f, 10f));
+        barEntries.add(new BarEntry(4f, 50f));
         BarDataSet barDataSet = new BarDataSet(barEntries, "BarDataSet");
         BarData barData = new BarData(barDataSet);
         barChart.setData(barData);
@@ -123,30 +69,7 @@ public class ProfileFragment extends Fragment {
         ImageView keepContributingImage = (ImageView) rootView.findViewById(R.id.keepContributingImage);
 
         //ImageViewBitmapLoader imageViewBitmapLoader = new ImageViewBitmapLoader(getContext());
-        imageViewBitmapLoader.loadBitmap(R.drawable.charity11,keepContributingImage);
+        imageViewBitmapLoader.loadBitmap(R.drawable.charity11, keepContributingImage);
         return rootView;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
